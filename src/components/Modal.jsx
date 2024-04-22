ccfa22f631fe496520e01e467773024779a62676
Divya Sharma <97099915+divs06@users.noreply.github.com>
2024-04-22 18:25:45 +0530

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -23,7 +23,7 @@ const Modal = ({ isOpen, onClose, children }) => {
                     width: 640,
                     margin: "auto",
                     padding: "2%",
-                    border: "2px solid #000",
+                    border: "1px solid #000",
                     borderRadius: "10px",
                     boxShadow: "2px solid black",
                 }}
